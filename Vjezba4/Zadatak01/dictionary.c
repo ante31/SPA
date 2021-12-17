1330898f443269b69cba60494a16f45d00c84485
ante31 <92745972+ante31@users.noreply.github.com>
2021-12-17 14:24:07 +0100

    Update dictionary.c

--- Vjezba4/Zadatak01/dictionary.c
+++ Vjezba4/Zadatak01/dictionary.c
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <string.h>
+
 // kreira novi prazni rjecnik
 Dictionary create()
 {
-	Dictionary w = (Dictionary)malloc(sizeof(Word));
-	w->word = " ";
-	w->count = 0;
-	w->next = NULL;
-	return w;
+	Dictionary dict = (Dictionary)malloc(sizeof(Word));
+	dict->word = "";
+	dict->count = 0;
+	dict->next = NULL;
+	return dict;
 }
 // briše cijeli rjeènik
 void destroy(Dictionary dict)
@@ -27,47 +28,49 @@ void destroy(Dictionary dict)
 // ispisuje sve rijeci i broj pojavljivanja svake rijeci
 void print(Dictionary dict)
 {
-	Dictionary s = dict;
-	s = s->next;
-	while (s != NULL)
+	Dictionary tmp = dict;
+	tmp = tmp->next;
+	while (tmp != NULL)
 	{
-		printf("%s", s->word);
-		printf(" %d\n", s->count);
-		s = s->next;
+		printf("%s", tmp->word);
+		printf(" %d\n", tmp->count);
+		tmp = tmp->next;
 	}
 }
 // dodaje rijec ili uvecava broj pojavljivanja rijeci u rjecniku
 // rijeci se dodaju u abecednom redu
 void add(Dictionary dict, char* str)
 {
-	Dictionary q = dict;
-	while (q->next != NULL)
+	Dictionary tmp = dict;
+	while (tmp->next != NULL)
 	{
-		if (strcmp(str, q->next->word) < 0)
+		if (strcmp(str, tmp->next->word) < 0)
 		{
 			Dictionary novi = (Dictionary)malloc(sizeof(Word));
 			novi->word = _strdup(str);
 			novi->count = 1;
-			novi->next = q->next;
-			q->next = novi;
+			novi->next = tmp->next;
+			tmp->next = novi;
 			return;
 		}
-		else if (strcmp(str, q->next->word) == 0)
+		else if (strcmp(str, tmp->next->word) == 0)
 		{
-			q->next->count++;
+			tmp->next->count++;
 			return;
 		}
-		q = q->next;
+		tmp = tmp->next;
 	}
-	q->next = (Dictionary)malloc(sizeof(Word));
-	q->next->word = _strdup(str);
-	q->next->count = 1;
-	q->next->next = NULL;
+	tmp->next = (Dictionary)malloc(sizeof(Word));
+	tmp->next->word = _strdup(str);
+	tmp->next->count = 1;
+	tmp->next->next = NULL;
 }
+
 int filter(Word* w)
 {
 	return (w->count >= 5 && w->count <= 10 && strlen(w->word) > 3);
 }
+
 Dictionary filterDictionary(Dictionary indict)
 {
 	Dictionary tmp = indict;
@@ -83,10 +86,10 @@ Dictionary filterDictionary(Dictionary indict)
 		}
 		else
 		{
-			Dictionary del = tmp->next;
+			Dictionary t = tmp->next;
 			tmp->next = tmp->next->next;
-			free(del->word);
-			free(del);
+			free(t->word);
+			free(t);
 		}
 	}
 	return indict;
